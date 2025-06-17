e02d601edcab446595dd71ca858734bf912532bf
BojkoS23 <BojkoS23@Z00T6-07>
2025-06-17 09:45:17 +0200

    bojkocommit

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const bojkoButton = document.getElementById('bojkobutton')
+const bojkoBackground = document.getElementById('bojkobackground')
 
 document.querySelector('h1').addEventListener('click', function() {
     alert('Header clicked!');
 });
+
+bojkoButton.addEventListener('click', function() {
+    bojkoBackground.style.backgroundColor = 'blue'
+
+})
