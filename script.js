618758dea8204c484b98b37ce37a3ce3f85535ed
DavidKminek <davidkminek@gmail.com>
2025-06-17 09:46:48 +0200

    oh boze

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 document.querySelector('h1').addEventListener('click', function() {
     alert('Header clicked!');
 });
+document.getElementById("kamo").addEventListener("click", function () {
+  this.style.backgroundColor = "#ffecb3";
+});
