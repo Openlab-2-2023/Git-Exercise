1e6036e2ceb41e1ef3281911da488f8f4a31b4d8
JanosikJ23 <JanosikJ23@Z00T6-12>
2025-06-17 10:41:35 +0200

    fix

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 document.querySelector('h1').addEventListener('click', function() {
     alert('Header clicked!');
 });
+
+
+
+const headerTitle = document.querySelector('header h1');
+
+headerTitle.addEventListener('click', () => {
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    headerTitle.style.color = randomColor;
+    console.log("Klikol si na meno Džordžo! Farba sa zmenila.");
+});
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log("🧙‍♀️ Vitaj na stránke Džordža!");
+});
